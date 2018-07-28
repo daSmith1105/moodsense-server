94f4bea9c39eb42c99fdf7aa7bd85fc81cafeca9
daSmith1105 <mustangdas@gmail.com>
2018-07-28 10:39:27 -0500

    added get, post, delete endpoints

--- apiRouter.js
+++ apiRouter.js
@@ -2,14 +2,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const { MoodEntry } = require('./moodlog/models');
+const { MoodEntry, Mood, User } = require('./models');
 
 const router = express.Router();
 
+/*
+sample entry
+{
+    "moods": [
+      {"moodType": "happy", "intensity": "5"},
+      {"moodType": "sad", "intensity": "3"}
+    ],
+    "note": "this is a sample note"
+}
+*/
 
-router.get('/moods', (req, res) => {
+// return all mood entries
+router.get('/mood-entries', (req, res) => {
+
     MoodEntry.find()
-    .populate('mood')
+      .populate('moods')
+      .populate('users')
       .then(entries => {
           res.json({ entries });
       })
@@ -19,8 +32,11 @@ router.get('/moods', (req, res) => {
   });
 });
 
-/*router.get('/moods/:id', (req, res) => {
+
+// this will get one mood entry and populate it with all the moods associated with it.
+router.get('/mood-entries/:id', (req, res) => {
     MoodEntry.findById(req.params.id)
+      .populate('moods')
       .then(entry => {
           res.json(entry);
       })
@@ -29,35 +45,52 @@ router.get('/moods', (req, res) => {
           res.status(500).json({message: 'Internal server error'});
   });
 });
-*/
 
-let obj = {
-    //created: 'now',
-    mood: [{moodType: 'happy', intensity: 5}, {moodType: 'sad', intensity: 3}],
-    note: 'this is a sample note'
-}
+// create a mood entry
+router.post('/mood-entries', (req, res) => {
+    console.log(req.body);
+    // create all the moods, ids of moods store those in the mood entry and save them to repopulate later
+    let moodRecords = [];
+    req.body.forEach(function (entry) {
+        entry.moods.forEach(function(mood) {
+            Mood.create(mood)
+            .then(moodRecord => {
+                console.log(moodRecord);
+                moodRecords.push(moodRecord._id);
+                console.log(moodRecords);
+                console.log(entry.moods);
+                if(moodRecords.length == entry.moods.length){
 
-router.post('/moods', (req, res) => {
-   return MoodEntry.create(obj)
-        .then(entry => {
-            return res.status(201).json(entry);
+                    // create the mood entry
+
+                    MoodEntry.create({note: entry.note, moods: moodRecords})
+                        .then(entryRecord => {
+                            console.log(entryRecord);
+                            return res.status(201).json(entryRecord);
+                        })
+                        .catch(err => {
+                            console.log(err);
+                        });
+
+                }
             })
-            .catch(err => {
-                console.log(err);
-        });
-    });
-/*
-router.put('/moods/:id', (req, res) => {
-    MoodEntry.findByIdAndUpdate()
     });
 
-router.delete('/moods/:id', (req, res) => {
+});
+
+});
+
+
+// delete a mood entry
+router.delete('/mood-entries/:id', (req, res) => {
+    console.log(req.id)
     MoodEntry.findByIdAndRemove(req.params.id)
     .then(() => res.status(204).end())
     .catch(err => {
         console.log(err);
-        res.status(500).json({message: 'Internal server error'}));
-    });
+        res.status(500).json({message: 'Internal server error'})
+    })
 });
-*/
+
+
   module.exports = router;
---
+++ models.js
@@ -0,0 +1,65 @@
+'use strict';
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const Schema = mongoose.Schema,
+ObjectId = Schema.ObjectId;
+mongoose.Promise = global.Promise;
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String, 
+    required: true
+  },
+  email: {type: String,  
+    required: true,
+    unique: true
+  }
+});
+
+userSchema.methods.serialize = function() {
+  return {
+    id: this._id || '',
+    username: this.username || '',
+    firstName: this.firstName || '',
+    email: this.email|| ''
+  };
+};
+
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compare(password, this.password);
+};
+
+userSchema.statics.hashPassword = function(password) {
+  return bcrypt.hash(password, 10);
+};
+
+
+const moodEntrySchema = mongoose.Schema({
+    user: { type: Schema.Types.ObjectId, ref: 'User' },
+    created: { type: Date, default: Date.now },
+    note: { type: String },
+    moods: [{ type: Schema.Types.ObjectId, ref: 'Mood' }]
+  });
+
+const moodSchema = mongoose.Schema({
+    user: { type: Schema.Types.ObjectId, ref: 'User' },
+    created: {type: Date, default: Date.now},
+    moodType: {type: String},
+    intensity: {type: String},
+  });
+
+
+const User = mongoose.model('User', userSchema);
+const MoodEntry = mongoose.model('MoodEntry', moodEntrySchema);
+const Mood = mongoose.model('Mood', moodSchema);
+
+module.exports = {MoodEntry, Mood, User};
